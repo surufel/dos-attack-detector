92474234da03dd19aef42178a7cb50ac4574b5a7
surufel <henriquefrancasmm@gmail.com>
2026-03-05 17:41:46 -0300

    change: better readability in echo outputs

--- traffic-monitor.sh
+++ traffic-monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "[*] Monitoring traffic for SYN package flooding . . ."
+echo "[/] Monitoring traffic for SYN package flooding . . ."
 THRESHOLD=20
 COOLDOWN=2
 LOG_FILE="/var/log/dos_detection.log"
@@ -8,12 +8,12 @@ while true; do
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S') # switch to TIMESTAMP=$(date '%d-%m-%Y %H:%M:S') for DAY/MONTH/YEAR time pattern
     SYN_COUNT=$(ss -tn state syn-recv | wc -l)
     
-    echo -n "[/] SYN_RECV Total: $SYN_COUNT"
+    echo -n "[*] SYN_RECV Total: $SYN_COUNT "
     
     if [ "$SYN_COUNT" -gt "$THRESHOLD" ]; then
         echo "[!] WARNING: Possible DoS has been detected at: "$TIMESTAMP". SYN COUNT: $SYN_COUNT" | tee -a "$LOG_FILE"
     else
-        echo "[*] Normal traffic. SYN COUNT: $SYN_COUNT"
+        echo "| [OK] Normal traffic. No threats detected."
     fi
     sleep "$COOLDOWN"
 done
