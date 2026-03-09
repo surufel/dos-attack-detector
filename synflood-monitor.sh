0633c380d5d7abb9337e3edf237d232008a50c06
surufel <henriquefrancasmm@gmail.com>
2026-03-09 10:29:20 -0300

    change: default THRESHOLD value changed, add comments for better understanding

--- synflood-monitor.sh
+++ synflood-monitor.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "[/] Monitoring traffic for SYN package flooding . . ."
-THRESHOLD=20
-COOLDOWN=2
-LOG_FILE="/var/log/dos_detection.log"
 
-# You'll need root for writting the logs in /var/log. 
+THRESHOLD=250 # Change this value depending on your traffic. Ideal values should be 2-3x the average of your traffic.
+COOLDOWN=2 # How often (in seconds) it'll check the traffic.
+LOG_FILE="/var/log/dos_detection.log" # If it detects a possible DoS attack, it will generate a log at the specified directory, in the specified file.
+
+# You'll need root for writting the logs in /var/log.
 if ! touch "$LOG_FILE" 2>/dev/null; then
     echo "[!] Error: cannot write to $LOG_FILE. Run as root."
     exit 1
@@ -15,8 +16,8 @@ trap "echo; echo '[*] Monitoring stopped.'; exit 0" SIGINT SIGTERM
 
 while true; do
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S') # switch to TIMESTAMP=$(date '+%d-%m-%Y %H:%M:%S') for DAY/MONTH/YEAR time pattern
-    SYN_COUNT=$(sudo timeout "$COOLDOWN" tcpdump -i any -nn 'tcp[tcpflags] & tcp-syn != 0 and tcp[tcpflags] & tcp-ack == 0' 2>/dev/null | grep -c '^[0-9]') 
-    if [ "$SYN_COUNT" -gt "$THRESHOLD" ]; then
+    SYN_COUNT=$(sudo timeout "$COOLDOWN" tcpdump -i any -nn 'tcp[tcpflags] & tcp-syn != 0 and tcp[tcpflags] & tcp-ack == 0' 2>/dev/null | grep -c '^[0-9]') # Uses tcpdump to listen the network, capturing each SYN package.
+    if [ "$SYN_COUNT" -gt "$THRESHOLD" ]; then # If the SYN package count is greater than the threshold, it will flag as a possible DoS, logging the detection in the LOG_FILE
         echo "[!] WARNING: Possible DoS detected at: $TIMESTAMP | SYN COUNT: $SYN_COUNT" | tee -a "$LOG_FILE"
     else
         echo "[OK] Total SYN Count: $SYN_COUNT. No threats detected."
