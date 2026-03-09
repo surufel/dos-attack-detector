d8c4eae491cbdf8dcea72a59895a09e1db28496a
surufel <henriquefrancasmm@gmail.com>
2026-03-09 08:41:43 -0300

    fix: add handling when not running as a sudo

--- synflood-monitor.sh
+++ synflood-monitor.sh
@@ -4,6 +4,12 @@ THRESHOLD=20
 COOLDOWN=2
 LOG_FILE="/var/log/dos_detection.log"
 
+# You'll need root for writting the logs in /var/log. 
+if ! touch "$LOG_FILE" 2>/dev/null; then
+    echo "[!] Error: cannot write to $LOG_FILE. Run as root."
+    exit 1
+fi
+
 while true; do
     TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S') # switch to TIMESTAMP=$(date '%d-%m-%Y %H:%M:S') for DAY/MONTH/YEAR time pattern
     SYN_COUNT=$(ss -tn state syn-recv | tail -n +2 | wc -l)
